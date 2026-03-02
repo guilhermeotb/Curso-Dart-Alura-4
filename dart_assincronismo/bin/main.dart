c6d2007f5bc0dce179e4cac6f2b790b677bef84c
Pereira <pereira@eb.mil.br>
2026-03-02 16:49:04 -0300

    #2 json capturado e decodificado

--- dart_assincronismo/bin/main.dart
+++ dart_assincronismo/bin/main.dart
@@ -1,4 +1,7 @@
+// dart run bin/main.dart
 import 'package:http/http.dart';
+import 'dart:convert';
+
 void main(){
   resquestData();
 }
@@ -6,4 +9,18 @@ void main(){
 resquestData(){
   String url = "https://gist.githubusercontent.com/guilhermeotb/b5e5988083befbf76bfc7e34c6f518d1/raw/8eb9c4615a551aa5f6eb119cec1a93c781759f5e/accounts.json";
  Future<Response> futureResponse = get(Uri.parse(url));
+ print(futureResponse);
+ futureResponse.then((Response response) {
+print(response);
+print(response.body);
+
+List<dynamic> listAccounts = json.decode(response.body);
+Map<String, dynamic> mapCarla = listAccounts.firstWhere((element) => element["name"] == "Carla",);
+
+print(mapCarla["balance"]);
+
+ },);
+
+
+ 
 }
