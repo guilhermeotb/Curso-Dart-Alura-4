175c460f718c775d4286a1c121cb8afff15d739d
Pereira <pereira@eb.mil.br>
2026-03-06 11:57:20 -0300

    #5 criação do account service e organização do código

---
+++ dart_assincronismo/lib/services/account_service.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+import 'package:dart_assincronismo/api_key.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class AccountService {  
+
+  StreamController<String> _streamController = StreamController<String>();
+  Stream<String> get streamInfos => _streamController.stream;
+  String url =
+   "https://api.github.com/gists/b5e5988083befbf76bfc7e34c6f518d1";
+
+
+  Future<List<dynamic>> resquestDataAsync() async {
+
+  Response response = await get(Uri.parse(url));
+  
+ _streamController.add("${DateTime.now()} | Requisição de leitura.");
+
+  return json.decode(response.body);
+
+}
+
+sendDataAsync(Map<String, dynamic> mapAccount) async{
+  List<dynamic> listAccounts = await resquestDataAsync();
+  listAccounts.add(mapAccount);
+  String content = json.encode(listAccounts);
+  
+
+   Response response = await post(
+    Uri.parse(url),
+    headers: {"Authorization" : "Bearer $githubApiKey"},
+    body: json.encode({
+    "description" : "account.json",
+    "public" : true,
+    "files" : {
+      "accounts.json" : { "content" : content,}
+    }
+    
+    }));
+
+   if(response.statusCode.toString()[0] == "2"){
+    _streamController.add("${DateTime.now()} | Requisição de adição bem sucedida (${mapAccount["name"]}).");
+   } else {
+    _streamController.add("${DateTime.now()} | Requisição falhou.");
+   }
+
+}
+
+}
